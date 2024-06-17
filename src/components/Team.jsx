ff9e6ee82ffe678687458be74939cb0d133bda6d
Melaniemsc <melsan1706@gmail.com>
2024-06-17 17:28:13 +0100

    updated the team component

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -4,15 +4,14 @@ import axios from "axios"
 
 
 const Team = () => {
+    const user = getPayload()
     const [team, setTeam] = useState([])
-    const [money, setMoney] = useState(100)
+    const [money, setMoney] = useState(`${user.usergameplay.money}`)
     const [strength, setStrength] = useState(0)
     const [intelligence, setIntelligence] = useState(0)
 
-    const user = getPayload()
-    console.log(user)
+    console.log(user.usergameplay.money)
 
-    
     const [characters, setCharacters] = useState([])
       
       
@@ -36,14 +35,17 @@ const Team = () => {
             const newTeam = structuredClone(team) 
             newTeam.push(character)
             setTeam(newTeam)
-            setMoney(money - character.cost)
-            setStrength(strength + character.strength)
-            setIntelligence(intelligence + character.intelligence)
+            const newMoney = money - character.cost
+            setMoney(newMoney)
+            const newStrength = strength + character.strength
+            setStrength(newStrength)
+            const newIntelligence = intelligence + character.intelligence
+            setIntelligence(newIntelligence)
 
-            // const token = localStorage.getItem('token')
-            // await axios.put('api/your-team', {money, strength, intelligence, team}, {
-            //     headers: { Authorization: `Bearer ${token}` }
-            // })
+            const token = localStorage.getItem('token')
+            await axios.put('api/your-team', {newMoney, newStrength, newIntelligence, newTeam}, {
+                headers: { Authorization: `Bearer ${token}` }
+            })
         }
     }
 
@@ -57,6 +59,23 @@ const Team = () => {
 
         <div>
             <h2 className="title">Team Members</h2>
+            <div className="columns is-multiline is-mobile">
+                {team.map((character) =>{
+                   return <div className="column is-one-third-desktop is-half-tablet is-half-mobile"
+                   key={character.name}>
+                       <p>{character.name}</p>
+                       {/* <figure className="image is-128x128">
+                           <img src={character.images.regular} alt={character.name}/>
+                       </figure> */}
+   
+                       <p>Cost: {character.cost}</p>
+                       <p>Strength: {character.strength}</p>
+                       <p>Intelligence: {character.intelligence}</p>
+                       <button className="button is-primary" onClick={() => handleAdd(character)}>Add</button>
+                   </div> 
+                })}
+            </div>
+
         </div>
 
         <div>
