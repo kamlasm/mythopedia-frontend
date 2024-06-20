f4edc6cf3f3d5357b4a4ba7c8c31f4cb683b924a
Kamla Mistry <kamla@mistry-family.co.uk>
2024-06-20 12:43:34 +0100

    update homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,36 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 const Home = () => {
-    return <div className="hero is-fullheight-with-navbar">
-      <div className="hero-body">
+
+  const location = useLocation()
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('token'))
+
+  useEffect(()=>{
+    setIsLoggedIn(localStorage.getItem('token'))
+  },[location])
+
+  return <div className="hero is-fullheight-with-navbar">
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <h1 className="title">
+          ⚡MYTHOPEDIA⚡
+        </h1>
         <div className="container has-text-centered">
-          <h1 className="subtitle mb-5">
-            Welcome to our Gods page, lets learn a bit more of the greek Gods
-          </h1>
-          <h2 className="title">
-          ⚡GODS⚡
-          </h2>
-          <div className="buttons is-centered" >
-          <Link to="/signup" className="button is-primary">Sign Up</Link>
-          <Link to="/login" className="button is-primary">Log In</Link>
-          </div>
+        <h1 className="subtitle mb-5">
+          Welcome to Mythopedia, the place to learn about Greek mythology.
+        </h1>
+        <h1 className="subtitle mb-5">
+          If you're up to the challenge, sign up to create a team of gods to battle against the deadliest monsters of Greek mythology.
+        </h1>
+        </div>
+        <div className="buttons is-centered" >
+          {!isLoggedIn && <Link to="/signup" className="button is-primary">Sign Up</Link>}
+          {!isLoggedIn && <Link to="/login" className="button is-primary">Log In</Link>}
         </div>
       </div>
     </div>
-  }
-  
-  export default Home
+  </div>
+}
+
+export default Home
